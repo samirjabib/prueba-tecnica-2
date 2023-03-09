efb3f89522180168ef49a4af09172b9f46820ace
samirjabib <samirdev9669@gmail.com>
2023-03-08 23:28:20 -0500

    add

--- client/src/dashboard/components/Sidebar/Sidebar.jsx
+++ client/src/dashboard/components/Sidebar/Sidebar.jsx
@@ -49,8 +49,8 @@ export const Sidebar = () => {
           xmlns="http://www.w3.org/2000/svg"
         >
           <path
-            clip-rule="evenodd"
-            fill-rule="evenodd"
+            clipRule="evenodd"
+            fillRule="evenodd"
             d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
           ></path>
         </svg>
@@ -68,10 +68,10 @@ export const Sidebar = () => {
             <li>
               <a
                 href="#"
-                class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+                className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
               >
                 
-                <div class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white">
+                <div className="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white">
                   Icon
                 </div>
                 <span className="ml-3">Dashboard</span>
--- client/src/dashboard/pages/ListBusiness.jsx
+++ client/src/dashboard/pages/ListBusiness.jsx
@@ -3,46 +3,35 @@ import React from "react";
 export const ListBusiness = () => {
   return (
     <>
-      <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-          <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+      <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
-              <th scope="col" class="px-6 py-3">
-                Product name
+              <th scope="col" className="px-6 py-3">
+                Name
               </th>
-              <th scope="col" class="px-6 py-3">
-                Color
+              <th scope="col" className="px-6 py-3">
+                Direction
               </th>
-              <th scope="col" class="px-6 py-3">
+              <th scope="col" className="px-6 py-3">
                 Category
               </th>
-              <th scope="col" class="px-6 py-3">
-                Price
-              </th>
-              <th scope="col" class="px-6 py-3">
-                <span class="sr-only">Edit</span>
+              <th scope="col" className="px-6 py-3">
+                nit
               </th>
             </tr>
           </thead>
           <tbody>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
               <th
                 scope="row"
-                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
               >
                 Name
               </th>
-              <td class="px-6 py-4">Direction</td>
-              <td class="px-6 py-4">Nit</td>
-              <td class="px-6 py-4">Phone</td>
-              <td class="px-6 py-4 text-right">
-                <a
-                  href="#"
-                  class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                >
-                  Edit
-                </a>
-              </td>
+              <td className="px-6 py-4">Direction</td>
+              <td className="px-6 py-4">Nit</td>
+              <td className="px-6 py-4">Phone</td>
             </tr>
           </tbody>
         </table>
