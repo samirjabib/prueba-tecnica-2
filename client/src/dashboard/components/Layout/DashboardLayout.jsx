d7d568f7bb3213833a8137e9f6fccc148e6d633c
samirjabib <samirdev9669@gmail.com>
2023-03-09 03:43:15 -0500

    add

--- client/src/dashboard/components/Layout/DashboardLayout.jsx
+++ client/src/dashboard/components/Layout/DashboardLayout.jsx
@@ -3,7 +3,7 @@ import { Sidebar } from "../Sidebar";
 
 export const DashboardLayout = ({ user, onLogout, theme, handleTheme }) => {
   return (
-    <div className="">
+    <div className="bg-white dark:bg-[#111827] h-screen">
       <Sidebar user={user} onLogout={onLogout} theme={theme} handleTheme={handleTheme}  />
       <div className="p-4 sm:ml-64 ">
         <Outlet />
